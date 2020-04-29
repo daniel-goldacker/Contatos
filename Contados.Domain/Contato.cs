08052d8b0b74b48f941e44e33f7e135ff8194e9e
Daniel Goldacker <daniel-goldacker@hotmail.com>
2020-04-29 19:10:47 -0300

    Retirada de Comentário Desnecessário
    Retirada de um comentário que foi realizado de forma desnecessária.

--- Contados.Domain/Contato.cs
+++ Contados.Domain/Contato.cs
@@ -8,21 +8,11 @@ namespace Contado.Domain
 {
     public class Contato
     {
-        /*
-          Criar um programa de agenda de contatos. O programa deve ter uma listagem onde é possível buscar o contato pelo nome.
-          O contato deve ter nome, endereço completo, telefone, celular e e-mail. Deve ter também uma opção para cadastrar novo 
-          contato. Ao clicar duas vezes sobre um contato na lista, abre a opção de detalhes do contato, podendo ser editado os
-          dados do mesmo. Deve permitir também excluir contato da lista. Definir um formato de arquivo e criar uma importação de 
-          contatos a partir de um arquivo texto (delimitado por tamanho de campo, por vírgula, fica aberto para escolher).     
-         */
-
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Endereco { get; set; }
         public string Telefone { get; set; }
         public string Celular { get; set; }
         public string  Email { get; set; }
-
-
     }
 }
